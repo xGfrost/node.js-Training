63ac8197149f59a42f1defc032535aafb8b55c9f
xGfrost <fadelakbar528@gmail.com>
2024-06-20 17:22:18 +0700

    node.js

--- training server/server.js
+++ training server/server.js
@@ -1,8 +1,18 @@
 const http = require('http');
 const fs = require('fs');
+const _ = require('lodash');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
+    
+    //lodash
+    const num = _.random(0, 20);
+    console.log(num);
+
+    const greet = _.once(() => {
+        console.log('hello');
+    });
+
+    greet();
 
     // set header content type
 res.setHeader('Content-Type', 'text/html');
@@ -11,12 +21,20 @@ let path = './views/';
 switch(req.url){
     case '/':
         path += 'index.html';
+        res.statusCode = 200;
         break;
         case '/about':
             path+= 'about.html';
+            res.statusCode = 200;
             break;
+            case '/about-me':            
+                res.statusCode = 301;
+                res.setHeader('Location', '/about')
+                res.end();
+                break;
             default:
                 path += '404.html';
+                res.statusCode = 404;
                 break;
 }
 
@@ -27,6 +45,7 @@ fs.readFile(path, (err, data) => {
         res.end();
     }else{
         // res.write(data);
+        
         res.end(data);
     }
 })
